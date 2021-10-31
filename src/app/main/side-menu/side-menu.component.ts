3219d7f2ab1e83160dd6fb7bccc8e74a8179211e
Vtrrix <viveksonybcd@gmail.com>
2021-10-31 19:55:19 +0530

    side menu bug fixes

--- src/app/main/side-menu/side-menu.component.ts
+++ src/app/main/side-menu/side-menu.component.ts
@@ -7,13 +7,16 @@ import { SideMenuService } from 'src/app/services/side-menu.service';
   styleUrls: ['./side-menu.component.css'],
 })
 export class SideMenuComponent implements OnInit {
-  menuElements: { title: string; route: string }[] = [];
+  menuElements: { title: string; route: string }[] = [
+    { title: 'My Info', route: '/' },
+    { title: 'My Status', route: 'status' },
+  ];
 
-  constructor(private sideMenuService: SideMenuService) {}
-
-  ngOnInit(): void {
+  constructor(private sideMenuService: SideMenuService) {
     this.sideMenuService.menuElements.subscribe((ele) => {
       this.menuElements = ele;
     });
   }
+
+  ngOnInit(): void {}
 }
