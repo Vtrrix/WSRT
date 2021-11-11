1f3bc74f16e299e3e27a3f442effdd682051550a
Vtrrix <viveksonybcd@gmail.com>
2021-11-11 21:58:52 +0530

    form setup 1

--- src/app/main/add-user/add-user.component.ts
+++ src/app/main/add-user/add-user.component.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import { team, TeamsService } from 'src/app/services/teams.service';
 
 @Component({
   selector: 'app-add-user',
   templateUrl: './add-user.component.html',
-  styleUrls: ['./add-user.component.css']
+  styleUrls: ['./add-user.component.css'],
 })
 export class AddUserComponent implements OnInit {
+  jobPositions: string[];
+  teams: team[];
+  managers: string[];
 
-  constructor() { }
+  selectedJob: string;
+  selectedTeam: team;
+  selectedManager: string;
 
-  ngOnInit(): void {
+  constructor(private teamsService: TeamsService) {
+    this.selectedJob = 'Select a Role';
+    this.selectedManager = 'Select a Manager';
+    this.selectedTeam = {
+      team_name: 'Select a Team',
+      managers: [],
+      description: '',
+      status_frequency: '',
+      team_short_name: '',
+    };
+    this.jobPositions = [
+      'SDE',
+      'Manager',
+      'big manager',
+      'bigger manager',
+      'biggest Manager',
+    ];
+    this.teams = [];
+    this.managers = [];
   }
 
+  ngOnInit(): void {
+    this.teamsService.getTeams().subscribe(
+      (res) => {
+        this.teams = res.data;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+  onJobSelect(job: string) {
+    this.selectedJob = job;
+  }
+  onTeamSelect(team: team) {
+    this.selectedTeam = team;
+  }
 }
