482be69035d15790c00e7d125caecfa7ad106bb9
Vtrrix <viveksonybcd@gmail.com>
2022-01-31 16:58:17 +0530

    added table component to teams list

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -10,9 +10,9 @@ export class TableComponent implements OnInit {
   @Input() columns: string[];
   @Input() data: Iterable<any>[];
   // for managerial view
-  @Input() username: string | null;
+  // @Input() username: string | null;
   @Input() inManagerView: boolean;
-  @Input() teamName: string;
+  // @Input() teamName: string;
   @Input() highlightRowList: boolean[];
 
   @Input() statusList: {
@@ -31,32 +31,12 @@ export class TableComponent implements OnInit {
     this.highlightRowList = [];
     this.statusList = [];
     this.data = [];
-    // To handle home component with manager view member
-
-    // 1. To check if manager checking member
     this.inManagerView = false;
-
-    // 2. If null will fetch current user data else provided user data
-    this.username = null;
-    //3. To use teamname in routing on specific status click
-    this.teamName = '';
-    // -----------------------------------------------
   }
 
   ngOnInit(): void {}
 
   onStatusClick(id: string) {
-    if (this.inManagerView) {
-      this.router.navigate([
-        '/user',
-        'manager',
-        'teams',
-        this.teamName,
-        this.username,
-        id,
-      ]);
-    } else {
-      this.router.navigate(['/user', 'status', id]);
-    }
+    // function
   }
 }
